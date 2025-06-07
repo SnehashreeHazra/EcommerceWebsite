5bf5c5570d1a90fe59c758596ba647d3b4d720da
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-06-07 20:37:01 +0530

    'sneha'

--- src/View/Contact/Contact.jsx
+++ src/View/Contact/Contact.jsx
@@ -4,10 +4,9 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div className="universal_container">
-      
       <div className="contact_us_content">
         <div className="contact_us_left">
-        <h2 className="contact_us_heading">Contact Us</h2>
+          <h2 className="contact_us_heading">Contact Us</h2>
           <div className="headquater_div">
             <div className="headquater_heading">
               <p>Headquater</p>
@@ -18,15 +17,17 @@ const Contact = () => {
               <p>Ahmedabad, India</p>
               <p>380015</p>
             </div>
-            <p className="working_hr">Working hours: Monday - Friday: 10:30 AM to 7 PM, IST</p>
+            <p className="working_hr">
+              Working hours: Monday - Friday: 10:30 AM to 7 PM, IST
+            </p>
           </div>
           <div className="contact_info_order">
             <div className="contact_info_div">
               <div className="contact_info_heading">
                 <p>Contact Info</p>
               </div>
-              <p style={{marginBottom:'5px'}}>example@sooti.com</p>
-              <p style={{marginBottom:'5px'}}>+123456789</p>
+              <p style={{ marginBottom: "5px" }}>example@sooti.com</p>
+              <p style={{ marginBottom: "5px" }}>+123456789</p>
               <div className="contact_social_icons">
                 <i class="fa-brands fa-facebook"></i>
                 <i class="fa-brands fa-square-instagram"></i>
@@ -37,9 +38,9 @@ const Contact = () => {
               <div className="order_info_heading">
                 <p>Order Information</p>
               </div>
-              <p style={{marginBottom:'5px'}}>Track Order</p>
-              <p style={{marginBottom:'5px'}}>Order Details</p>
-              <p style={{marginBottom:'5px'}}>Shipping Policy</p>
+              <p style={{ marginBottom: "5px" }}>Track Order</p>
+              <p style={{ marginBottom: "5px" }}>Order Details</p>
+              <p style={{ marginBottom: "5px" }}>Shipping Policy</p>
             </div>
           </div>
         </div>
@@ -47,20 +48,44 @@ const Contact = () => {
           <h4>Write Us</h4>
           <form className="contact_form">
             <div className="form-group">
-              <p>Name <span>*</span></p>
-              <input type="text" className="form-control" placeholder="Enter your name" required />
+              <p>
+                Name <span>*</span>
+              </p>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your name"
+                required
+              />
             </div>
             <div className="form-group">
-              <p>Email <span>*</span></p>
-              <input type="text" className="form-control" placeholder="Enter your email" required />
+              <p>
+                Email <span>*</span>
+              </p>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your email"
+                required
+              />
             </div>
             <div className="form-group">
               <p>Phone Number</p>
-              <input type="text" className="form-control" placeholder="+91 123456789" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="+91 123456789"
+              />
             </div>
             <div className="form-group">
-              <p>What's on your mind? <span>*</span></p>
-              <textarea className="form-control" placeholder="Write your message here..." required></textarea>
+              <p>
+                What's on your mind? <span>*</span>
+              </p>
+              <textarea
+                className="form-control"
+                placeholder="Write your message here..."
+                required
+              ></textarea>
             </div>
           </form>
         </div>
