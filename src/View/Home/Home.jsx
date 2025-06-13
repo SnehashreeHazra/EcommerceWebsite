4882eb08fc88bab1aa06ef43efa83a847919cf0e
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-06-13 23:21:26 +0530

    'up'

--- src/View/Home/Home.jsx
+++ src/View/Home/Home.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import HomeBanner from './HomeBanner'
-import './Home.css'
-import HomeCategories from './HomeCategories'
-import InSpotlight from './InSpotlight'
-import HomeParallax from './HomeParallax'
-import Quality from './Quality'
-import Testimonial from './Testimonial'
-import Footer from '../../Layout/Footer'
-import NewArrivals from './NewArrivals'
+import React from "react";
+import HomeBanner from "./HomeBanner";
+import "./Home.css";
+import HomeCategories from "./HomeCategories";
+import InSpotlight from "./InSpotlight";
+import HomeParallax from "./HomeParallax";
+import Quality from "./Quality";
+import Testimonial from "./Testimonial";
+import Footer from "../../Layout/Footer";
+import NewArrivals from "./NewArrivals";
 const Home = () => {
   return (
     <>
-      <HomeBanner/>
-      <HomeCategories/>
-      <NewArrivals/>
-      <InSpotlight/>
-      <HomeParallax/>
-      <Quality/>
-      <Testimonial/>
+      <HomeBanner />
+      <HomeCategories />
+      <NewArrivals />
+      <InSpotlight />
+      <HomeParallax />
+      <Quality />
+      <Testimonial />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
