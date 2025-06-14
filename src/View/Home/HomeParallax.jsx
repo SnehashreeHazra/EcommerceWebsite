417e8577673016d0e51b7e5fbbf9e1a7c2e92788
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-06-14 23:56:03 +0530

    'up'

--- src/View/Home/HomeParallax.jsx
+++ src/View/Home/HomeParallax.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 const HomeParallax = () => {
   return (
     <>
-      <div className='parallax_wrapper'>
-            <div className='parallax_div'>
-               <h5>Limited Time Offer</h5>
-               <h3>Special Edition</h3>
-               <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
-ullamcorper mattis, pulvinar dapibus leo.</h6>
-<h4>Buy This Saree At 20% Discount, Use Code OFF20</h4>
+      <div className="parallax_wrapper">
+        <div className="parallax_div">
+          <h5>Limited Time Offer</h5>
+          <h3>Special Edition</h3>
+          <h6>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </h6>
+          <h4>Buy This Saree At 20% Discount, Use Code OFF20</h4>
 
-<button className='shop_now_btn'>Shop Now</button>
-            </div>
+          <button className="shop_now_btn">Shop Now</button>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default HomeParallax
+export default HomeParallax;
