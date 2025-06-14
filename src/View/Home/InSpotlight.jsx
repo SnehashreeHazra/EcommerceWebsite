b5b444843bb736f10e027ff31b47cec7e292e626
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-06-14 23:56:38 +0530

    'up'

--- src/View/Home/InSpotlight.jsx
+++ src/View/Home/InSpotlight.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import './Home.css'
-import SpotlightCard from './SpotlightCard'
-import { useNavigate } from 'react-router-dom'
-const InSpotlight = ({realatedProducts}) => {
+import React from "react";
+import "./Home.css";
+import SpotlightCard from "./SpotlightCard";
+import { useNavigate } from "react-router-dom";
+const InSpotlight = ({ realatedProducts }) => {
   const navigate = useNavigate();
   return (
     <>
-    <div className='spotlight_wrapper home_categories_wrapper universal_container'>
-    <div className='home_categories_head'>
-     <h1>
-     
-     FEATURED PRODUCTS
-     </h1>
-     <span className='line_through'></span>
+      <div className="spotlight_wrapper home_categories_wrapper universal_container">
+        <div className="home_categories_head">
+          <h1>FEATURED PRODUCTS</h1>
+          <span className="line_through"></span>
+        </div>
+        <div className="spotlight_products_wrapper">
+          <SpotlightCard realatedProducts={true} />
+        </div>
+        {!realatedProducts && (
+          <div
+            className="view_all_button"
+            onClick={() => navigate("/products")}
+          >
+            View All
+          </div>
+        )}
       </div>
-      <div className='spotlight_products_wrapper'>
-        <SpotlightCard realatedProducts={true}/>
-      </div>
-      {! realatedProducts && <div className='view_all_button' onClick={() => navigate('/products')}>View All</div>}
-    </div>
-     
     </>
-  )
-}
+  );
+};
 
-export default InSpotlight
+export default InSpotlight;
