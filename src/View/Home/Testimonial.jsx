e56e7f0a4219c8cd01c51217eeec1d8b175bd9c6
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-06-15 23:36:56 +0530

    'up'

--- src/View/Home/Testimonial.jsx
+++ src/View/Home/Testimonial.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
-import TestimonialCard from './TestimonialCard'
+import React from "react";
+import TestimonialCard from "./TestimonialCard";
 
 const Testimonial = () => {
   return (
     <>
-    <div className='testimonial_wrapper home_categories_wrapper universal_container'>
-    <div className='home_categories_head'>
-      <h1>
-     
-    CUSTOMER TESTIMONIALS
-     </h1>
-     <span className='line_through'></span>
-      </div>
+      <div className="testimonial_wrapper home_categories_wrapper universal_container">
+        <div className="home_categories_head">
+          <h1>CUSTOMER TESTIMONIALS</h1>
+          <span className="line_through"></span>
+        </div>
 
-      <div className='testimonial_cards_wrapper'>
-        <TestimonialCard/>
+        <div className="testimonial_cards_wrapper">
+          <TestimonialCard />
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Testimonial
+export default Testimonial;
