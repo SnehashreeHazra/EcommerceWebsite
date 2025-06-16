5f1c4a79e6d288b8369c19615b735b7359633a5a
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-06-16 23:35:24 +0530

    'up'

--- src/View/Products/Products.jsx
+++ src/View/Products/Products.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import './Products.css'
-import FilterProducts from './FilterProducts'
-import ProductsList from './ProductsList'
+import React from "react";
+import "./Products.css";
+import FilterProducts from "./FilterProducts";
+import ProductsList from "./ProductsList";
 
 const Products = () => {
   return (
-    <div className='universal_container all-products-page'>
+    <div className="universal_container all-products-page">
       <FilterProducts />
       <ProductsList />
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
